ad776cdd9b6e9562cb6f54572ac8d5cfbe5c3f9e
Kingsley Victor <javaprodigy56@gmail.com>
2019-04-04 13:18:49 +0100

    Included pagination feature

--- src/main/java/com/gitangular/graph/resolvers/Query.java
+++ src/main/java/com/gitangular/graph/resolvers/Query.java
@@ -1,10 +1,15 @@
 package com.gitangular.graph.resolvers;
 
+import java.util.List;
+
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.gitangular.graph.models.User;
 import com.gitangular.graph.repositories.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -39,5 +44,12 @@ public class Query implements GraphQLQueryResolver {
         } else {
             throw new Exception("User not found");
         }
+    }
+
+    public List<User> getAllUsers(Integer page) {
+        Pageable pageable = PageRequest.of(page, 12);
+        Page<User> paged = userRepository.findAll(pageable);
+
+        return paged.getContent();
     }
 }
